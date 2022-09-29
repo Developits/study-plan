a807615bc8ef1ecacfd73e0f22ae0be643b9fc79
Developits <nahidnabil1234@gmail.com>
2022-09-29 21:00:12 +0600

    Home page started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import "./App.css";
+import React from "react";
 import Header from "./components/Header/Header";
+import Questions from "./components/Questions/Questions";
 import Tasklist from "./components/Tasklist/Tasklist";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header></Header>
       <Tasklist></Tasklist>
+      <Questions></Questions>
     </div>
   );
 }
